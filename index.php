ac1646b028d1f951fc4d3a3e3984f027454a0642
eisha <eishashaukat219@gmail.com>
2024-03-28 13:31:40 +0500

    responisve banner

--- index.php
+++ index.php
@@ -51,11 +51,11 @@ $a = 'active';
   </div>
   <div class="car-btn">
   <button class="carousel-control-prev " type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon  " aria-hidden="true"><i class="fa fa-chevron-left"></i></span>
+    <span class="carousel-control  " aria-hidden="true"><i class="fa fa-chevron-left"></i></span>
     <span class="visually-hidden ">Previous</span>
   </button>
   <button class="carousel-control-next  " type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"><i class="fa fa-chevron-right"></i></span>
+    <span class="carousel-control" aria-hidden="true"><i class="fa fa-chevron-right"></i></span>
     <span class="visually-hidden">Next</span>
   </button>
   </div>
